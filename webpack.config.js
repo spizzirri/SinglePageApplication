32bdd440dfad41feedb587f23d9201977d1c9341
Spizzirri <damianspizzirri@gmail.com>
2021-03-06 19:34:59 -0300

    Corrige parametro que se manda a HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -22,10 +22,10 @@ module.exports = {
         ]
     },
     plugins:[
-        new HtmlWebpackPlugin([{
+        new HtmlWebpackPlugin({
             inject: true,
             template:'./public/index.html',
             filename: './index.html'
-        }])
+        })
     ]
 }
